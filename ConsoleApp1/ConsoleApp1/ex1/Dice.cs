7a706514f46ae5ebd00f3b3f3ac0550012ebc5ed
tientvgcd201862 <115529220+tientvgcd201862@users.noreply.github.com>
2023-01-10 14:52:34 +0700

    Ex1 - Submission

---
+++ ConsoleApp1/ConsoleApp1/ex1/Dice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.ex1
+{
+    internal class Dice
+    {
+        private int side;
+
+        public int Side
+        {
+            set { if (value > 0) side = value; }
+            get { return side; }
+        }
+
+        public void SetSide(int side)
+        {
+            if (side > 0)
+            {
+                this.side = side;
+            }
+            else
+            {
+                side = 6;
+            }
+        }
+
+        public int Roll()
+        {
+            Random rd = new Random();
+            return rd.Next(1, side + 1);
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/ex1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dice dicel = new Dice();
+            dicel.Side = 5;
+            Console.WriteLine($"the number of side: " + dicel.Side);
+            Console.WriteLine("the result of running the dice: " + dicel.Roll());
+            Console.ReadLine();
+        }
+    }
+}
