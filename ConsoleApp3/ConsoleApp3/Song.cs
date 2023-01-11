314aa31f19f2fbe84f46964b373fdef9af7e6bf7
tientvgcd201862 <115529220+tientvgcd201862@users.noreply.github.com>
2023-01-11 15:13:34 +0700

    EX3

---
+++ ConsoleApp3/ConsoleApp3/Song.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Song
+    {
+        private string typeList;
+        private string name;
+        private string time;
+
+        public string TypeList
+        {
+            set { typeList = value; }
+            get { return typeList; }
+        }
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+        public string Time
+        {
+            set { time = value; }
+            get { return time; }
+        }
+
+        public Song()
+        {
+
+        }
+        public Song(string typeList, string name, string time)
+        {
+            TypeList = typeList;
+            Name = name;
+            Time = time;
+        }
+    }
+}
+
