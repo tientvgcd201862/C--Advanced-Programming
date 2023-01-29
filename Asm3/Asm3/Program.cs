87075b34dc142ed121ded4b071ed5638e7a9fdff
tientvgcd201862 <115529220+tientvgcd201862@users.noreply.github.com>
2023-01-29 14:00:39 +0700

    Add files via upload
    hw3

---
+++ Asm3/Asm3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asm3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Function function = new Function();
+            Console.WriteLine("Choose the function below");
+            function.Menu();
+            Console.Write("Enter your choice: ");
+            int choice = int.Parse(Console.ReadLine());
+            
+            do
+            {
+                if (choice == 1)
+                {
+                    function.AddAStudentToList();
+                }
+                else if (choice == 2)
+                {
+                    Console.Write("Enter student name: ");
+                    string studentName = Console.ReadLine();
+                    
+                    function.FindByName(studentName);
+                }
+                else if (choice == 3)
+                {
+                    Console.WriteLine("End the program!");
+                    
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice! Please try again!");
+                }
+                function.Menu();
+                Console.Write("Enter your choice: ");
+                choice = int.Parse(Console.ReadLine());
+            }
+            while (true);
+            Console.ReadLine();
+        }
+    }
+}
