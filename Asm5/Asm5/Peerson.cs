b812a6ef6d96fffd159d8c77c2855f944e371ed8
tientvgcd201862 <115529220+tientvgcd201862@users.noreply.github.com>
2023-02-01 19:58:16 +0700

    Add files via upload
    hw5

---
+++ Asm5/Asm5/Peerson.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asm5
+{
+    internal class Peerson
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+        public decimal Salary { get; private set; }
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age > 30)
+                this.Salary += this.Salary * percentage / 100;
+            else
+                this.Salary += this.Salary * percentage / 200;
+        }
+        
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} is {Age} years old.Salary is {Salary}";
+        }
+
+        internal void Add(Peerson person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Peerson(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary= salary;
+        }
+
+        public Peerson()
+        {
+        }
+      
+    }
+}
+
---
+++ Asm5/Asm5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Asm5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of person: ");
+            var lines = int.Parse(Console.ReadLine());
+            var people = new List<Peerson>();
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                Console.WriteLine("first name: ");
+                var FirstName = Console.ReadLine();
+                Console.WriteLine("last name: ");
+                var LastName = Console.ReadLine();
+                Console.WriteLine("age: ");
+                var Age = int.Parse(Console.ReadLine());
+                Console.WriteLine("salary: ");
+                var Salary = int.Parse(Console.ReadLine());
+                Peerson person = new Peerson(FirstName, LastName, Age, Salary);
+                people.Add(person);
+                Console.WriteLine(person.ToString());
+                Console.ReadLine();
+                
+
+                // Create variables for constructor parameters
+                // Initialize a Person
+                // Add it to the list
+
+            }
+            Console.WriteLine("sorted by name and age");
+            var sorted = people.OrderBy(p => p.FirstName).ThenBy(p => p.Age).ToList();
+            Console.WriteLine(string.Join(Environment.NewLine, sorted));
+
+
+        }
+    }
+}
