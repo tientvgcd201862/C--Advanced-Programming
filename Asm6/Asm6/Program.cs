c5a3b1974de34d5e077ec91d0666df8713afd5b0
tientvgcd201862 <115529220+tientvgcd201862@users.noreply.github.com>
2023-02-06 11:28:39 +0700

    lab
    lab

---
+++ Asm6/Asm6/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Asm6
+{
+
+    internal class Program
+    {
+       
+        public class Animal
+        {
+            public void Eat()
+            {
+                Console.WriteLine("Eating...");
+            }
+        }
+        public class Dog:Animal
+        {
+            public void Bark()
+            {
+                Console.WriteLine("Braking");
+            }
+        }
+
+        public class Puppy:Animal
+        {
+            public void Bark() { }
+            public void Weep()
+            {
+                Console.WriteLine("weeping");
+            }
+        }
+        public class Cat:Animal
+        {
+            public void Bark() { }
+            public void Weep() { }
+            public void Eat() { }
+            public void Meow() {
+                Console.WriteLine("meowing...");
+            }
+        }
+        
+
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Eat();
+            dog.Bark();
+            dog.Bark();
+            Console.WriteLine("..........");
+            Puppy puppy = new Puppy();
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
+            Console.WriteLine("..........");
+            Dog dog1 = new Dog();
+            dog1.Eat();
+            dog1.Bark();
+            Cat cat = new Cat();
+            cat.Eat();
+            cat.Meow();
+        }
+    }
+}
