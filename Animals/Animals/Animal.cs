3e2eeaac89b0396839a0e775abeaadf30b406d70
tientvgcd201862 <115529220+tientvgcd201862@users.noreply.github.com>
2023-02-09 16:25:03 +0700

    lab
    lab

---
+++ Animals/Animals/Animal.cs
@@ -0,0 +1,22 @@
+﻿namespace Animals
+{
+    public abstract class Animal
+    {    // Create Constructor
+
+        protected Animal(string name, string favouriteFood)
+        {
+            Name = name;
+            FavouriteFood = favouriteFood;
+        }
+
+        public string Name { get; private set; }
+        public string FavouriteFood { get; private set; }
+        public virtual string ExplainSelf()
+        {
+            return string.Format(
+              "I am {0} and my favourite food is {1}",
+              this.Name,
+              this.FavouriteFood);
+        }
+    }
+}
