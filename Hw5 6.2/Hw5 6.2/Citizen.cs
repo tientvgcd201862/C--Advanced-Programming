66e08e4c285864b1190c876275f962ee53a2de26
tientvgcd201862 <115529220+tientvgcd201862@users.noreply.github.com>
2023-02-12 22:01:48 +0700

    Hw
    hw

---
+++ Hw5 6.2/Hw5 6.2/Citizen.cs
@@ -0,0 +1,20 @@
+﻿namespace Hw5_6._2
+{
+    internal class Citizen
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string ID { get; set; }
+        public string Birthday { get; set; }
+        public Citizen(string s, string name, int age, string id, string birthday)
+        {
+            Name = name;
+            Age = age;
+            ID = id;
+            Birthday = birthday;
+        }
+        public Citizen()
+        {
+        }
+    }
+}
---
+++ Hw5 6.2/Hw5 6.2/Pet.cs
@@ -0,0 +1,18 @@
+﻿namespace Hw5_6._2
+{
+    internal class Pet
+    {
+        public string Name { get; set; }
+        public string Birthday { get; set; }
+
+        public Pet(string s, string name, string birthday)
+        {
+            Name = name;
+            Birthday = birthday;
+        }
+
+        public Pet()
+        {
+        }
+    }
+}
